251a67dd4bc97cd21a3b0df56cf649a429044c1b
Manish Kumar <k.manu00005@gmail.com>
2024-04-21 19:56:30 +0530

    Refactoring the service module i.e FakeStoreProductServiceImpl by seperating Mapping and 3rd party api call

--- src/main/java/com/example/productservicejanbatch/services/FakeStoreProductServiceImpl.java
+++ src/main/java/com/example/productservicejanbatch/services/FakeStoreProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.productservicejanbatch.dtos.FakeStoreProductDto;
 import com.example.productservicejanbatch.exceptions.ProductNotFoundException;
 import com.example.productservicejanbatch.models.Category;
 import com.example.productservicejanbatch.models.Product;
+import com.example.productservicejanbatch.thirdpartyclients.FakeStoreClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -19,56 +20,24 @@ import java.util.LinkedList;
 import java.util.List;
 @Service("FakeProductService")
 public class FakeStoreProductServiceImpl implements ProductService{
-    //restTemplateBuilder is a webclient provided by spring this is use for http connection
-    private RestTemplateBuilder restTemplateBuilder;
 
-    //private String getProductUrl = "https://fakestoreapi.com/products/1";//now we need clint to make a call at this url so we use restTemplateBuilder
-    private String specificProductUrl = "https://fakestoreapi.com/products/{id}";
-    private String genericProductUrl = "https://fakestoreapi.com/products";
+    private FakeStoreClient fakeStoreClient;
     @Autowired
-    public FakeStoreProductServiceImpl(RestTemplateBuilder restTemplateBuilder){
-        this.restTemplateBuilder = restTemplateBuilder;
+    public FakeStoreProductServiceImpl(FakeStoreClient fakeStoreClient){
+        this.fakeStoreClient = fakeStoreClient;
     }
 
-//    @Override
-//    public String getProductById(Long id) {
-//
-//        RestTemplate restTemplate = restTemplateBuilder.build();
-//        ResponseEntity<String> responseEntity = restTemplate.getForEntity(getProductUrl, String.class);
-//
-//        //return "Product Fetch From Fake Service. Id: " + id;
-//        return "Product Fetch From Fake Service. Id: " + responseEntity.toString();
-//    }
-//    @Override
-//    public Product getProductById(Long id) {
-//
-//        RestTemplate restTemplate = restTemplateBuilder.build();
-//        ResponseEntity<Product> responseEntity = restTemplate.getForEntity(getProductUrl, Product.class);
-//
-//        //return "Product Fetch From Fake Service. Id: " + id;
-//        return  responseEntity.getBody();
-//    }
     @Override
     public Product getProductById(Long id) throws ProductNotFoundException {
 
-    RestTemplate restTemplate = restTemplateBuilder.build();
-    ResponseEntity<FakeStoreProductDto> responseEntity = restTemplate.getForEntity(specificProductUrl, FakeStoreProductDto.class, id);
-
-    if(responseEntity.getBody() == null){
-        //throw an Exception, So we should make our own exception to show the msg
-        throw new ProductNotFoundException("Product Not found for id :" +id);
-    }
-    //return "Product Fetch From Fake Service. Id: " + id;
-    return  getProductFromFakeStoreProductDto(responseEntity.getBody());
+        return  getProductFromFakeStoreProductDto(fakeStoreClient.getProductById(id));
 }
 
     @Override
     public List<Product> getAllProducts() {
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<FakeStoreProductDto[]> responseEntity = restTemplate.getForEntity(genericProductUrl, FakeStoreProductDto[].class);
-        
+
         List<Product> productList = new LinkedList<>();
-        for(FakeStoreProductDto fakeStoreProductDto: responseEntity.getBody()){
+        for(FakeStoreProductDto fakeStoreProductDto: fakeStoreClient.getAllProducts()){
             productList.add(getProductFromFakeStoreProductDto(fakeStoreProductDto));
         }
 
@@ -82,52 +51,14 @@ public class FakeStoreProductServiceImpl implements ProductService{
 
     @Override
     public Product deleteProductById(Long id) {
-        // Prepare a RestTemplate instance to execute HTTP requests
-        RestTemplate restTemplate = restTemplateBuilder.build();
-
-        // Prepare a request callback to set the accept header for the HTTP request
-        RequestCallback requestCallback = restTemplate.acceptHeaderRequestCallback(FakeStoreProductDto.class);
-
-        // Prepare a response extractor to extract the response entity from the HTTP response
-        ResponseExtractor<ResponseEntity<FakeStoreProductDto>> responseExtractor = restTemplate.responseEntityExtractor(FakeStoreProductDto.class);
-
-        // Execute an HTTP DELETE request to the specific product URL with the provided ID
-        ResponseEntity<FakeStoreProductDto> responseEntity = restTemplate.execute(
-                specificProductUrl,             // URL of the specific product to be deleted
-                HttpMethod.DELETE,             // HTTP DELETE method
-                requestCallback,               // Request callback to set accept header
-                responseExtractor,             // Response extractor to extract response entity
-                id                             // ID of the product to be deleted
-        );
-
-        // Convert the response body (deleted product) from FakeStoreProductDto to Product object
-        Product deletedProduct = getProductFromFakeStoreProductDto(responseEntity.getBody());
-
-        // Return the deleted product
-        return deletedProduct;
+       return getProductFromFakeStoreProductDto(fakeStoreClient.deleteProductById(id));
     }
-
-//    public Product deleteProductById(Long id) {
-//        RestTemplate restTemplate = restTemplateBuilder.build();
-//        //restTemplate.delete(specificProductUrl, id);
-//        //we get response in JSON format from http request below in requestCallback
-//        RequestCallback requestCallback = restTemplate.acceptHeaderRequestCallback(FakeStoreProductDto.class);
-//        ResponseExtractor<ResponseEntity<FakeStoreProductDto>> responseExtractor = restTemplate.responseEntityExtractor(FakeStoreProductDto.class);
-//
-//        ResponseEntity<FakeStoreProductDto> responseEntity = restTemplate.execute(specificProductUrl, HttpMethod.DELETE, requestCallback, responseExtractor, id);
-//
-//        return getProductFromFakeStoreProductDto(responseEntity.getBody());
-//    }
 
 
     @Override
     public Product addProduct(Product product) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<FakeStoreProductDto> responseEntity = restTemplate.postForEntity(genericProductUrl,
-                getFakeStoreProductDtoFromProduct(product), FakeStoreProductDto.class);
-
-        return getProductFromFakeStoreProductDto(responseEntity.getBody());
+        return getProductFromFakeStoreProductDto(fakeStoreClient.addProduct(getFakeStoreProductDtoFromProduct(product)));
     }
 
     @Override
@@ -187,3 +118,27 @@ public class FakeStoreProductServiceImpl implements ProductService{
  * So write exceptional handler in controller
  *
 * */
+
+
+/**
+ * FakeStoreProductServiceImpl is doing lot of work such as:
+ * a> Handle the 3rd party call
+ * b> Mapping
+ *
+ * Now which is business logic?
+ * Mapping is business logic for service module. So put 3rd party handling into another
+ *
+ */
+
+/**
+ * So we Refactor our code to make a> Mapping i.e our business logic for Service module and b> Handle the 3rd party call
+ * There for we make another module called as thirdpartyclient for Handle the 3rd party call.
+ * This will make our code clean in service file.
+ * Now if tomarrow say our client i.e url(fakestore api) changes the format our service will be least impacted and we only need to change the
+ * Mapping logic i.e getFakeStoreProductDtoFromProduct and getProductFromFakeStoreProductDto rest every this will be
+ * as it is.
+ *
+ *This is a Variation of Adapter Design Pattern so we separate both:
+ *  * a> Handle the 3rd party call
+ *  * b> Mapping
+ */
